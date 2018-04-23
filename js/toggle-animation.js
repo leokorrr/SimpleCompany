a474bc9fda810b316cc438adcac2224c8be3f3af
leokorrr <leokorrr@gmail.com>
2018-04-23 19:15:26 +0200

    animations for index

---
+++ js/toggle-animation.js
@@ -0,0 +1,33 @@
+var i = 0;
+$('#check-projects').click(function () {
+    i++;
+    if (i > 3) {
+        $('#check-projects').html("Stop clicking!");
+        $('#check-projects').animate({
+            marginLeft: "5px",
+        }, 100);
+        $('#check-projects').animate({
+            marginLeft: "-5px",
+        }, 100);
+        $('#check-projects').animate({
+            marginLeft: "5px",
+        }, 100);
+        $('#check-projects').animate({
+            marginLeft: "-5px",
+        }, 100);
+    } else {
+        $('#check-projects').animate({
+            marginLeft: "5%",
+        }, 400);
+        $('#check-projects').animate({
+            marginLeft: "0%",
+        }, 400);
+        $('#check-projects').animate({
+            marginLeft: "5%",
+        }, 400);
+        $('#check-projects').animate({
+            marginLeft: "0%",
+        }, 400);
+    }
+
+})
