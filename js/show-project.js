1b35f175a84a4c7c6266b66081b377b38ebfbee3
leokorrr <leokorrr@gmail.com>
2018-04-29 22:56:04 +0200

    update 29/04/2018

--- js/show-project.js
+++ js/show-project.js
@@ -1,13 +1,48 @@
+$('#close-project').hide();
 $('.project-card').click(function(){
-  $('.project-card').not(this).hide(1000);
-  $(this).animate({
-    width: "75%",
-    position: "absolute",
-    zIndex: "9999"
+  $('.project-card').not(this).stop(true,true).hide(500);
+  $('#close-project').show();
+
+  var elementWidth = $(this).width()/$(this).parent().width()*100;
+  if(elementWidth != 75){
+  $(".navbar").hide();
+  $(this).delay(1000).stop(true,false).animate({
+    width: "75vw",
+    position: "absolute"
   }, 1000);
-  $('.purple-layout').hide();
+
+  $('.purple-layout').fadeOut();
+
   var id = '#' + $(this).attr('id') + '-data';
-  $(id).delay(900).animate({
-    width:"25%"
+  $(id).delay(1900).stop(true,false).animate({
+    width:"25vw"
   },1000);
+
+  }
+
+
+
+  $('#close-functionality').attr('closure-attr', $(this).attr('id'));
+
+  $('#close-project').click(function(){
+    // $('#close-project').hide(1000);
+    var elementId = '#' + $('#close-functionality').attr('closure-attr');
+    var elementDataId = elementId + '-data';
+    $('#close-project').hide();
+
+    $(elementId).delay(1000).stop(true, false).animate({
+      width: "25vw",
+      position: "absolute",
+      zIndex: "9998"
+    }, 1000);
+
+    $('.purple-layout').fadeIn();
+
+    var id = '#' + $(this).attr('id') + '-data';
+    $(elementDataId).stop(true, false).animate({
+      width:"0vw"
+    },1000);
+    $('.project-card').not(elementId).delay(1000).show(1000);
+    $('.navbar').delay(1000).css('display', 'block');
+  });
 });
